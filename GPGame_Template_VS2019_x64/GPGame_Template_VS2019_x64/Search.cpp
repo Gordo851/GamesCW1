f4d91fc04ddbc1e1fcea84ff64e9df2931fb6fc4
Gordon Rennie <46560768+Gordo851@users.noreply.github.com>
2020-02-16 19:54:41 +0000

    search class files

---
+++ GPGame_Template_VS2019_x64/GPGame_Template_VS2019_x64/Search.cpp
@@ -0,0 +1,17 @@
+#include "Search.h"
+#include <iostream>
+#include <sstream>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+Node::Node()
+{
+
+}
+
+Node::~Node()
+{
+
+}
---
+++ GPGame_Template_VS2019_x64/GPGame_Template_VS2019_x64/Search.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+class Node {
+public:
+	Node();
+	~Node();
+	glm::vec2 index = glm::vec2(0, 0);
+	glm::vec2 parent = glm::vec2(0, 0);
+	float HeuristicCost = 0.0f;
+	float pathCost = 1.0f;
+	bool travelUp = false;
+	bool travelDown = false;
+	bool travelLeft = false;
+	bool travelRight = false;
+	
+};
